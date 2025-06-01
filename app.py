9ec050c0f79e89e7287cbacecacde5a8b331deb8
Aniruddha Shete <sheteaniruddha8@gmail.com>
2025-06-01 19:32:10 +0530

    movie recommendation function

--- app.py
+++ app.py
@@ -8,3 +8,12 @@ import google.generativeai as genai
 API_KEY = ""
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash")
+
+# Function to recommend movies
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = [movies.iloc[i[0]].title for i in movies_list]
+    return recommended_movies
